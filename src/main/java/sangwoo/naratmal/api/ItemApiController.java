466b598b102bfd8ef6bda3f945191982853544e0
sangwoo <tkddn3367@gmail.com>
2022-05-15 16:34:39 +0900

    fetch Item By Id API

---
+++ src/main/java/sangwoo/naratmal/api/ItemApiController.java
@@ -0,0 +1,35 @@
+package sangwoo.naratmal.api;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import sangwoo.naratmal.model.domain.Item;
+import sangwoo.naratmal.model.dto.ApiResult;
+import sangwoo.naratmal.repository.ItemRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class ItemApiController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping("/item/{itemId}")
+    public ApiResult selectItem(@PathVariable Long itemId) {
+        Item item = itemRepository.findById(itemId);
+        return new ApiResult(new SelectItemResponse(item));
+    }
+
+    @Data
+    private static class SelectItemResponse {
+        private String title;
+        private String description;
+        private Long likes;
+
+        public SelectItemResponse(Item item) {
+            title = item.getTitle();
+            description = item.getDescription();
+        }
+    }
+}

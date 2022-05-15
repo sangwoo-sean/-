dbfc0ab198ff5ccb14c9e1c1bdc72d2f9260a957
sangwoo <tkddn3367@gmail.com>
2022-05-15 12:16:06 +0900

    init

---
+++ src/main/java/sangwoo/naratmal/NaratmalApplication.java
@@ -0,0 +1,13 @@
+package sangwoo.naratmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaratmalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaratmalApplication.class, args);
+	}
+
+}

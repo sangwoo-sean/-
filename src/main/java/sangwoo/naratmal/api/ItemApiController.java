d3d233af24c5fdb1f78ae4bedaec6d7078a464b7
sangwoo <tkddn3367@gmail.com>
2022-05-22 15:33:00 +0900

    create Item Admin

--- src/main/java/sangwoo/naratmal/api/ItemApiController.java
+++ src/main/java/sangwoo/naratmal/api/ItemApiController.java
@@ -3,9 +3,7 @@ package sangwoo.naratmal.api;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sangwoo.naratmal.model.domain.Item;
 import sangwoo.naratmal.model.dto.ApiResult;
 import sangwoo.naratmal.repository.ItemRepository;
@@ -22,6 +20,14 @@ public class ItemApiController {
         return new ApiResult(new SelectItemResponse(item));
     }
 
+    @PostMapping("/api/item")
+    @Transactional
+    public ApiResult createItem(@RequestBody CreateItemRequest request) {
+        Item item = Item.createItem(request.getTitle(), request.getDescription());
+        Long savedId = itemRepository.save(item);
+        return new ApiResult(savedId);
+    }
+
     @Data
     private static class SelectItemResponse {
         private String title;
@@ -32,5 +38,11 @@ public class ItemApiController {
             title = item.getTitle();
             description = item.getDescription();
         }
+    }
+
+    @Data
+    private static class CreateItemRequest {
+        private String title;
+        private String description;
     }
 }

df3a5610c5b70863e03ddb904795e89dc9296650
sangwoo <tkddn3367@gmail.com>
2022-05-22 15:06:20 +0900

    admin pages Read

--- src/main/java/sangwoo/naratmal/controller/AdminController.java
+++ src/main/java/sangwoo/naratmal/controller/AdminController.java
@@ -1,26 +1,47 @@
 package sangwoo.naratmal.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sangwoo.naratmal.model.domain.AmendRequest;
+import sangwoo.naratmal.model.domain.Item;
+import sangwoo.naratmal.model.domain.NewRequest;
+import sangwoo.naratmal.repository.AmendRequestRepository;
+import sangwoo.naratmal.repository.NewRequestRepository;
+import sangwoo.naratmal.service.ItemService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final ItemService itemService;
+    private final NewRequestRepository newRequestRepository;
+    private final AmendRequestRepository amendRequestRepository;
+
     @GetMapping("/items")
-    public String adminHome() {
-        return "admin/data-table";
+    public String adminHome(Model model) {
+        List<Item> itemList = itemService.findAll();
+        model.addAttribute("itemList", itemList);
+        return "admin/item-list";
     }
 
     @GetMapping("/new-requests")
-    public String newRequestList() {
-        return "admin/data-table";
+    public String newRequestList(Model model) {
+        List<NewRequest> requestList = newRequestRepository.findAll();
+        model.addAttribute("requestList", requestList);
+        return "admin/new-request-list";
     }
 
     @GetMapping("/amend-requests")
-    public String amendRequestList() {
-        return "admin/data-table";
+    public String amendRequestList(Model model) {
+        List<AmendRequest> requestList = amendRequestRepository.findAll();
+        model.addAttribute("requestList", requestList);
+        return "admin/amend-request-list";
     }
 
     @GetMapping("/login")
